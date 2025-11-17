c118ccd29b8e9154fddd29b5bc935bdf8369c7d8
ashibullah <ashibullahbhai@gmail.com>
2025-11-17 11:40:58 +0600

    Image optimization

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import nextthump from "@/public/images/nextjs.jpeg";
 
 export default function Home() {
   return (
     <div>
       <h1>Hello World. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, minima beatae enim pariatur officiis veniam perferendis hic quidem exercitationem recusandae amet architecto aut! Saepe veniam nemo harum, accusantium consequuntur quo.</h1>
+      <Image  src={nextthump} alt="thumbnail" width={'400'} quality={60} />
     </div>
   )
 }
